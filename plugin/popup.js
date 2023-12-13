bc4ae2467af26667f6f3aa04b04d7141dca79976
anya-mb <anyamb@yandex.ru>
2023-12-13 14:58:13 -0800

    fixed popup.js file where the code was executed twice for 1 click

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,60 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("reverseText").addEventListener("click", async function () {
-        chrome.tabs.executeScript({
-            code: `window.getSelection().toString();`
-        }, async function (selection) {
-            const selectedText = selection[0];
-
-            // Call service to reverse text
-            const response = await fetch("https://bcnhtyp368.execute-api.us-east-1.amazonaws.com/reverse", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ text: selectedText, 'operation': 'reverse' })
-            });
-
-            const data = await response.json();
-
-            const reversedText = data.text;
-
-            // Insert reversed text
+// Check if event listeners have already been added
+if (!window.hasEventListenersAdded) {
+    document.addEventListener("DOMContentLoaded", function () {
+        // Event listener for reverseText button
+        document.getElementById("reverseText").addEventListener("click", function () {
             chrome.tabs.executeScript({
-                code: `
-                    document.execCommand('insertText', false, "${reversedText}");
-                `
+                code: 'window.getSelection().toString();'
+            }, async function (selection) {
+                const selectedText = selection[0];
+
+                // Call service to reverse text
+                const response = await fetch("https://bcnhtyp368.execute-api.us-east-1.amazonaws.com/reverse", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ text: selectedText, 'operation': 'reverse' })
+                });
+
+                const data = await response.json();
+                const reversedText = data.text;
+
+                // Insert reversed text
+                chrome.tabs.executeScript({
+                    code: `document.execCommand('insertText', false, "${reversedText}");`
+                });
+            });
+        });
+
+        // Event listener for summarizeText button
+        document.getElementById("summarizeText").addEventListener("click", function () {
+            chrome.tabs.executeScript({
+                code: 'window.getSelection().toString();'
+            }, async function (selection) {
+                const selectedText = selection[0];
+
+                // Call service to summarize text
+                const response = await fetch("https://bcnhtyp368.execute-api.us-east-1.amazonaws.com/summarize", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ text: selectedText, 'operation': 'summarize' })
+                });
+
+                const data = await response.json();
+                const summarizedText = data.text;
+
+                // Insert summarized text
+                chrome.tabs.executeScript({
+                    code: `document.execCommand('insertText', false, "${summarizedText}");`
+                });
             });
         });
     });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("summarizeText").addEventListener("click", async function () {
-        chrome.tabs.executeScript({
-            code: `window.getSelection().toString();`
-        }, async function (selection) {
-            const selectedText = selection[0];
-
-            // Call service to reverse text
-            const response = await fetch("https://bcnhtyp368.execute-api.us-east-1.amazonaws.com/summarize", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ text: selectedText, 'operation': 'summarize' })
-            });
-
-            const data = await response.json();
-
-            const summarizedText = data.text;
-
-            // Insert reversed text
-            chrome.tabs.executeScript({
-                code: `
-                    document.execCommand('insertText', false, "${summarizedText}");
-                `
-            });
-        });
-    });
-});
-
+    window.hasEventListenersAdded = true;
+}
